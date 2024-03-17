f16470dccad20d6ec6855c28ec4234fa363f1a9e
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-03-17 02:40:58 -0500

    Added localStorage event adding

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,10 +1,29 @@
+//setting up localStorage namespaces if missing
+if (!localStorage.getItem('tasks')){
+    localStorage.setItem('tasks', '[]')
+}
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+let taskIds = getIds()
+let newTaskID = 0
+
+//pulls used ID's into an array and returns it for taskID variable
+function getIds(){
+    const localArray = []
+    for(const task in taskList){
+        localArray.push(taskList[task].id)
+    }
+    return localArray
+}
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+//return new task id to handleAddTask
+//if the list of ID's does not contain the saved next ID var then return the nextID var and set a new nextID
+//else generate a new id
 }
 
 // Todo: create a function to create a task card
@@ -18,8 +37,23 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(){
+    const taskName = document.getElementById('task-input').value
+    const taskDate = document.getElementById('task-date').value
+    const taskDesc = document.getElementById('task-desc').value
+    const djDate = `${dayjs(taskDate).$M + 1}/${dayjs(taskDate).$D}/${dayjs(taskDate).$y}`
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    const newTask = {
+        date: djDate,
+        name: taskName,
+        desc: taskDesc,
+        id: generateTaskId()
+    }
 
+    tasks.push(newTask)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    console.log(taskName, taskDesc, djDate )
+    console.log(dayjs(djDate))
 }
 
 // Todo: create a function to handle deleting a task
@@ -34,5 +68,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
 });
+
+//adding function for button click MIGHT CHANGE
